49a1f02ae683fd09d1ac54021796df577c0b28f1
Goldenlion5648 <coboudinot@gmail.com>
2020-02-14 18:41:17 -0500

    fixed bounding box showing when looking at nothing

--- Assets/Scripts/MemoryGameScipt.cs
+++ Assets/Scripts/MemoryGameScipt.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using sys = System;
 
 public class MemoryGameScipt : MonoBehaviour
 {
@@ -12,29 +13,37 @@ public class MemoryGameScipt : MonoBehaviour
     int currentDim = 3;
     int numFilledIn = 0;
 
-    float showedTime =0 ;
+    float showedTime = 0;
     int numToAdd = -1;
+
+    int lastCubeLookedAtNum = 0;
 
 
     string cubePartName = "cubePart";
     string textPartName = "textNum";
     string filledInTag = "filledIn";
+    string userClicked = "userClicked";
 
     bool isShowing = true;
 
     Color cubeColor = new Color(0, 0, 255);
 
+    Texture2D lookingAtTexture;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        originalCube =  GameObject.Find("OGCube");
+        originalCube = GameObject.Find("OGCube");
+        originalCube.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
 
         //player = Instantiate(Sce,
         //            new Vector3(originalCube.transform.position.x + x * offsetFromEachOther,
         //            originalCube.transform.position.y + offsetFromOriginal + y * offsetFromEachOther,
         //            originalCube.transform.position.z),
         //            Quaternion.identity);
+        lookingAtTexture = (Texture2D)Resources.Load("squareOutline");
+
 
         setup();
     }
@@ -46,23 +55,34 @@ public class MemoryGameScipt : MonoBehaviour
 
     void makePattern()
     {
+        for (int i = 0; i < currentDim * currentDim; i++)
+        {
+            GameObject.Find(cubePartName + i).GetComponent<cubeProperties>().userClicked = false;
+            GameObject.Find(cubePartName + i).transform.GetComponent<Renderer>().material.color = new Color(255, 255, 255);
+        }
+
+
         numFilledIn = 0;
-        while(numFilledIn < currentDim * currentDim / 3 + numToAdd)
+        while (numFilledIn < currentDim * currentDim / 3 + numToAdd)
         {
             int randomTile = Random.Range(0, currentDim * currentDim);
             Transform currentObject = GameObject.Find(cubePartName + randomTile).transform;
 
-            if (currentObject.tag.Contains(filledInTag) == false)
+            if (currentObject.GetComponent<cubeProperties>().isFilledIn == false)
             {
-                currentObject.tag = filledInTag;
+                currentObject.GetComponent<cubeProperties>().isFilledIn = true;
+                //Debug.Log(currentObject.tag);
 
                 Renderer render = currentObject.GetComponent<Renderer>();
                 render.material.color = cubeColor;
                 numFilledIn++;
 
+                Debug.Log("correct tile: " + currentObject.name);
+
             }
         }
         showedTime = Time.time;
+        isShowing = true;
 
         Debug.Log("showedTime: " + showedTime);
     }
@@ -71,7 +91,7 @@ public class MemoryGameScipt : MonoBehaviour
     void setup()
     {
         int total = 0;
-        for (int y = currentDim; y >0 ; y--)
+        for (int y = currentDim; y > 0; y--)
         {
             for (int x = 0; x < currentDim; x++)
             {
@@ -86,6 +106,8 @@ public class MemoryGameScipt : MonoBehaviour
                     Quaternion.identity);
 
                 newCube.name = cubePartName + total;
+                newCube.name = cubePartName + total;
+
                 //newCube.transform.tag = "";
                 text.transform.parent = newCube.transform;
 
@@ -100,8 +122,59 @@ public class MemoryGameScipt : MonoBehaviour
                 total += 1;
             }
         }
+
+        Destroy(originalCube);
         makePattern();
 
+    }
+
+    void compareAnswer()
+    {
+        int numCorrect = 0;
+        int total = 0;
+        for (int y = currentDim; y > 0; y--)
+        {
+            for (int x = 0; x < currentDim; x++)
+            {
+                if (GameObject.Find(cubePartName + total).GetComponent<cubeProperties>().userClicked == true)
+                {
+                    if (GameObject.Find(cubePartName + total).GetComponent<cubeProperties>().isFilledIn == true)
+                    {
+                        numCorrect++;
+                    }
+                    else
+                    {
+                        //y = -1;
+                        return;
+                    }
+                }
+                else
+                {
+                    if (GameObject.Find(cubePartName + total).GetComponent<cubeProperties>().isFilledIn == false)
+                    {
+                        return;
+                    }
+                }
+
+                total++;
+            }
+        }
+
+        if (numCorrect >= numFilledIn)
+        {
+            numToAdd += 1;
+            if (numToAdd == 2)
+            {
+                numToAdd = -1;
+                currentDim += 1;
+            }
+
+            Debug.Log("Making new pattern");
+            Debug.Log("numCorrect:" + numCorrect);
+            Debug.Log("numFilledIn:" + numFilledIn);
+            makePattern();
+
+        }
     }
 
     // Update is called once per frame
@@ -118,25 +191,68 @@ public class MemoryGameScipt : MonoBehaviour
             isShowing = false;
         }
 
-        //if (isShowing == false)
+        if (isShowing == false)
         {
-            if (Input.GetMouseButtonDown(0))
+
+            RaycastHit target;
+
+            //Gizmos.DrawLine
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out target, 25))
             {
-                RaycastHit target;
-
-                //Gizmos.DrawLine
-                if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out target, 25) && target.collider.name.Contains(cubePartName))
+                //Debug.Log(target);
+                if (target.collider.name.Contains(cubePartName))
                 {
-                Debug.DrawLine(Camera.main.transform.position, target.transform.position, Color.black, 5);
+                    //Debug.Log("Looking at: " + target.transform.name);
 
-                    Renderer r = target.transform.gameObject.GetComponent<Renderer>();
+                    if (lastCubeLookedAtNum != sys.Int32.Parse(target.transform.name.Substring(cubePartName.Length)))
+                    {
 
-                    r.material.SetColor("_Color", Color.blue);
+                        GameObject.Find(cubePartName + lastCubeLookedAtNum).GetComponent<Renderer>().material.mainTexture = Texture2D.whiteTexture;
+                        Renderer rend = target.collider.gameObject.GetComponent<Renderer>();
+                        target.collider.gameObject.GetComponent<cubeProperties>().isBeingLookedAt = true;
+                    }
 
-                    Debug.Log("Changed Color");
+                    lastCubeLookedAtNum = sys.Int32.Parse(target.transform.name.Substring(cubePartName.Length));
+                    GameObject.Find(cubePartName + lastCubeLookedAtNum).GetComponent<Renderer>().material.mainTexture = lookingAtTexture;
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        //Debug.Log("Hit: " + target.transform.name);
+
+                        target.collider.gameObject.GetComponent<cubeProperties>().userClicked =
+                            !target.collider.gameObject.GetComponent<cubeProperties>().userClicked;
+                        //Debug.DrawLine(Camera.main.transform.position, target.transform.position, Color.black, 5);
+
+                        Renderer r = target.transform.gameObject.GetComponent<Renderer>();
+
+                        if (r.material.GetColor("_Color") == Color.blue)
+                        {
+                            r.material.SetColor("_Color", Color.white);
+
+                        }
+                        else
+                        {
+                            r.material.SetColor("_Color", Color.blue);
+
+                        }
+
+                        Debug.Log("Changed Color of " + target.transform.name);
+                    }
+                }
+                else
+                {
+                    //GameObject.Find(cubePartName + lastCubeLookedAtNum).GetComponent<Renderer>().material.mainTexture = Texture2D.whiteTexture;
 
                 }
+
+                compareAnswer();
+            }
+            else
+            {
+                GameObject.Find(cubePartName + lastCubeLookedAtNum).GetComponent<Renderer>().material.mainTexture = Texture2D.whiteTexture;
+
             }
         }
+
+
     }
 }
---
+++ Assets/Scripts/cubeProperties.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeProperties : MonoBehaviour
+{
+    public bool isFilledIn = false;
+    public bool userClicked = false;
+    public bool isBeingLookedAt = false;
+
+
+
+}
